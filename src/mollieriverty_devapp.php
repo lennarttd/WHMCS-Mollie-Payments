e98d43a21c97985eaa34f5c452d7b83bca369296
Lennart Dreves <lennart@lennmedia.nl>
2025-12-08 00:05:20 +0100

    Added Riverty payment method

---
+++ src/mollieriverty_devapp.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/mollie/mollie.php';
+
+function mollieriverty_devapp_config()
+{
+    $config = mollie_config();
+
+    $config = array_merge($config, array(
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Mollie Riverty'
+        )
+    ));
+
+    return $config;
+}
+
+function mollieriverty_devapp_link($params)
+{
+    return mollie_link($params, \Mollie\Api\Types\PaymentMethod::RIVERTY);
+}

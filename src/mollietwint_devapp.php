3cba71bc535d27209bea5a118f77cbb9a0ba48b2
Lennart Dreves <lennart@lennmedia.nl>
2025-12-08 00:06:36 +0100

    Added TWINT payment method

---
+++ src/mollietwint_devapp.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/mollie/mollie.php';
+
+function mollietwint_devapp_config()
+{
+    $config = mollie_config();
+
+    $config = array_merge($config, array(
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Mollie TWINT'
+        )
+    ));
+
+    return $config;
+}
+
+function mollietwint_devapp_link($params)
+{
+    return mollie_link($params, \Mollie\Api\Types\PaymentMethod::TWINT);
+}
